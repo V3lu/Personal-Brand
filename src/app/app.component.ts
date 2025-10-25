a990aa7f55ca4ebdb4c98bbeada52cef3e9c8551
Adam Wielogórski <71398159+Khangineer@users.noreply.github.com>
2025-10-25 09:28:21 +0200

    Tailored logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { MainComponent } from "./main/main.component";
+import { AboutMeComponent } from "./about-me/about-me.component";
+import { SkillsComponent } from "./skills/skills.component";
+import { MyWorkComponent } from "./my-work/my-work.component";
+import { ContactComponent } from "./contact/contact.component";
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+    styleUrl: './app.component.css',
+    imports: [MainComponent, AboutMeComponent, SkillsComponent, MyWorkComponent, ContactComponent]
 })
 export class AppComponent {
   
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-main',
-    imports: [],
     templateUrl: './main.component.html',
     styleUrl: './main.component.css'
 })
