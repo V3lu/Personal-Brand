7b39890a318cd44d57d1b27043c28555a4dd5522
Adam Wielogórski <71398159+Khangineer@users.noreply.github.com>
2025-10-26 09:10:41 +0100

    91010262025

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { MainComponent } from "./main/main.component";
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { AboutMeComponent } from "./about-me/about-me.component";
 import { SkillsComponent } from "./skills/skills.component";
 import { MyWorkComponent } from "./my-work/my-work.component";
@@ -9,8 +8,42 @@ import { ContactComponent } from "./contact/contact.component";
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [MainComponent, AboutMeComponent, SkillsComponent, MyWorkComponent, ContactComponent]
+    imports: [AboutMeComponent, SkillsComponent, MyWorkComponent, ContactComponent]
 })
 export class AppComponent {
   
+    @ViewChild('home')
+    homeRef: ElementRef | undefined;
+
+    @ViewChild('about')
+    aboutRef: ElementRef | undefined;
+
+    @ViewChild('skills')
+    skillsRef: ElementRef | undefined;
+
+    @ViewChild('mywork')
+    myWorkRef: ElementRef | undefined;
+
+    @ViewChild('contact')
+    contactRef: ElementRef | undefined;
+
+    ScrollToHome(){
+        this.homeRef?.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
+    }
+
+    ScrollToAbout(){
+        this.aboutRef?.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
+    }
+
+    ScrollToSkills(){
+        this.skillsRef?.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
+    }
+
+    ScrollToMyWork(){
+        this.myWorkRef?.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
+    }
+
+    ScrollToContact(){
+        this.contactRef?.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
+    }
 }
