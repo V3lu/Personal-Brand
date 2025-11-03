192ba9c7a065dbb547761b0ac23223c6b5c2d13a
Adam Wielogórski <71398159+Khangineer@users.noreply.github.com>
2025-11-03 12:35:46 +0100

    Contact in progress

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -8,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class ContactComponent {
 
+
+    RedirectToGH(){
+        window.open("https://github.com/V3lu", "_blank")
+    }
+
+    RedirectToLI(){
+        window.open("https://www.linkedin.com/in/adam-krzysztof-wielog%C3%B3rski-217ba8321/", "_blank")
+    }
+
+    RedirectToX(){
+        window.open('https://x.com/_AdamWi', '_blank');
+    }
 }
